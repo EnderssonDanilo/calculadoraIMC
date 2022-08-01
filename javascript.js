07365df5bac4e23d6a735141becf3c42e6f1bd4e
Endersson Danilo <endersondanilo@gmail.com>
2022-08-01 12:57:46 -0300

    adcionado todo html, css e JS da calculadora iMC

--- javascript.js
+++ javascript.js
@@ -1,8 +1,29 @@
-const circulo = document.querySelector("#circulo");
-
-function seguirMouse(event) {
-  circulo.style.top = event.y + "px";
-  circulo.style.left = event.x + "px";
+function validaEntrada(args) {
+  for (let i = 0; i < arguments.length; i++) {
+    if (!!arguments[i] == false || arguments[i] < 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-window.addEventListener("mousemove", seguirMouse);
+function calcularIMC(kilos, altura) {
+  altura = altura / 100;
+  return kilos / (altura * altura);
+}
+
+const formCalcularIMC = document.getElementById("form");
+
+formCalcularIMC.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const kilos = parseFloat(document.getElementById("kilos").value);
+  const altura = parseFloat(document.getElementById("altura").value);
+
+  if (validaEntrada(kilos, altura)) {
+    const imc = calcularIMC(kilos, altura);
+    document.getElementById("imc").value = parseInt(imc).toFixed(2);
+  } else {
+    document.getElementById("imc").value = "## ERRO ##";
+  }
+});
